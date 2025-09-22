a8bd5dc844d93730464ddf876da65a7802bbb488
Yoon Do Yeong <ehdud05264@naver.com>
2025-09-22 19:31:52 +0900

    4장_문자열 결합 연산자

---
+++ 4장/ex_StringConcatenate/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ex_StringConcatenate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = "123" + "456";
+            Console.WriteLine(result);  // 123456
+
+            result = "Hello" + " " + "World!";
+            Console.WriteLine(result);  // Hello World!
+        }
+    }
+}

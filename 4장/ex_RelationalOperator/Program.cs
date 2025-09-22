58049434df88e91984f9ee8fd640e30076bc545c
Yoon Do Yeong <ehdud05264@naver.com>
2025-09-22 20:25:56 +0900

    4장_관계 연산자

---
+++ 4장/ex_RelationalOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ex_RelationalOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"3 > 4 : {3 > 4}");      // 3 > 4 : False
+            Console.WriteLine($"3 >= 4 : {3 >= 4}");    // 3 >= 4 : False
+            Console.WriteLine($"3 < 4 : {3 < 4}");      // 3 < 4 : True
+            Console.WriteLine($"3 <= 4 : {3 <= 4}");    // 3 <= 4 : True
+            Console.WriteLine($"3 == 4 : {3 == 4}");    // 3 == 4 : False
+            Console.WriteLine($"3 != 4 : {3 != 4}");    // 3 != 4 : True
+
+        }
+    }
+}

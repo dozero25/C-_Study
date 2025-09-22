a21870798312dd4e7fde81078e0e1ba19e471530
Yoon Do Yeong <ehdud05264@naver.com>
2025-09-22 20:27:37 +0900

    4장_논리연산자

---
+++ 4장/ex_LogicalOperator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using static System.Console;
+
+namespace ex_LogicalOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Testing && ...");
+            WriteLine($"1 > 0 && 4 < 5 : {1 > 0 && 4 < 5}");        
+            WriteLine($"1 > 0 && 4 > 5 : {1 > 0 && 4 > 5}");
+            WriteLine($"1 == 0 && 4 > 5 : {1 == 0 && 4 > 5}");
+            WriteLine($"1 == 0 && 4 < 5 : {1 == 0 && 4 < 5}");
+
+            WriteLine("\nTesting ||  ...");
+            WriteLine($"1 > 0 || 4 < 5 : {1 > 0 || 4 < 5}");
+            WriteLine($"1 > 0 || 4 > 5 : {1 > 0 || 4 > 5}");
+            WriteLine($"1 == 0 || 4 > 5 : {1 == 0 || 4 > 5}");
+            WriteLine($"1 == 0 || 4 < 5 : {1 == 0 || 4 < 5}");
+
+            WriteLine("\nTesting !  ...");
+            WriteLine($"!True : {!true}");
+            WriteLine($"!False : {!false}");
+
+            /*
+            
+            Testing && ...
+            1 > 0 && 4 < 5 : True
+            1 > 0 && 4 > 5 : False
+            1 == 0 && 4 > 5 : False
+            1 == 0 && 4 < 5 : False
+
+            Testing ||  ...
+            1 > 0 || 4 < 5 : True
+            1 > 0 || 4 > 5 : True
+            1 == 0 || 4 > 5 : False
+            1 == 0 || 4 < 5 : True
+
+            Testing !  ...
+            !True : False
+            !False : True
+             
+             */
+        }
+    }
+}

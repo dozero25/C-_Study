664a348067ef47d09677097b925fada0847b779e
Yoon Do Yeong <ehdud05264@naver.com>
2025-09-22 12:35:57 +0900

    4장_산술연산자

---
+++ 4장/ex_ArithmaticOperators/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex_ArithmaticOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 111 + 222;
+            Console.WriteLine($"a : {a}");  // a : 333
+
+            int b = a - 100;
+            Console.WriteLine($"b : {b}");  // b : 233
+
+            int c = b * 10;
+            Console.WriteLine($"c : {c}");  // c : 2330
+
+            double d = c / 6.3;
+            Console.WriteLine($"d : {d}");  // d : 369.8412698412699
+
+            Console.WriteLine($"22 / 7 = {22 / 7}({22 % 7})");  // 22 / 7 = 3(1)
+        }
+    }
+}

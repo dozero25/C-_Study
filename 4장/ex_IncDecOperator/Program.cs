6668f854fa8f1b8908575df5ded307c9cb22bed2
Yoon Do Yeong <ehdud05264@naver.com>
2025-09-22 18:31:27 +0900

    4장_증감 연산자

---
+++ 4장/ex_IncDecOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ex_IncDecOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a++); // 10 <- 현재 값 출력 후 ++
+            // a는 현재 11
+            Console.WriteLine(++a); // 12 <- 바로 적용
+
+            Console.WriteLine(a--); // 12
+            Console.WriteLine(--a); // 10
+        }
+    }
+}

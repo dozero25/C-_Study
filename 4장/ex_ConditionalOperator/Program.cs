51dcb3ef9603fb0db537b16c23c7465384d68575
Yoon Do Yeong <ehdud05264@naver.com>
2025-09-23 19:23:31 +0900

    4장_조건 연산자

---
+++ 4장/ex_ConditionalOperator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ex_ConditionalOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String result = (10 % 2) == 0 ? "짝수" : "홀수";
+            Console.WriteLine(result);    // 짝수
+        }
+    }
+}

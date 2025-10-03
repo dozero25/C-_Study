1e1bef9bd522ca68fb87c7ac05784bec57acfff5
Yoon Do Yeong <ehdud05264@naver.com>
2025-10-03 11:19:24 +0900

    7장_this

---
+++ 7장/ex_This/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ex_This
+{
+    class Employee
+    {
+        private string Name;
+        private string Position;
+
+        public void SetName(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetPosition(string Position)
+        {
+            this.Position = Position;
+        }
+
+        public string GetPosition()
+        {
+            return this.Position;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee pooh = new Employee();
+            pooh.SetName("Pooh");
+            pooh.SetPosition("Waiter");
+            Console.WriteLine($"{pooh.GetName()} {pooh.GetPosition()}");    // Pooh Waiter
+
+            Employee tigger = new Employee();
+            tigger.SetName("Tigger");
+            tigger.SetPosition("Cleaner");
+            Console.WriteLine($"{tigger.GetName()} {tigger.GetPosition()}");// Tigger Cleaner
+
+        }
+    }
+}

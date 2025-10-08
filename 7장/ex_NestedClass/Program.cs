c6fbfeef8a891ab332c8636df242434db8abe72f
Yoon Do Yeong <ehdud05264@naver.com>
2025-10-08 11:46:12 +0900

    7장_클래스

---
+++ 7장/ex_NestedClass/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ex_NestedClass
+{
+    internal class Program
+    {
+        class Configuration
+        {
+            List<ItemValue> listConfig = new List<ItemValue>();
+
+            // 새로운 설정을 추가하거나, 기존 설정 업데이트
+            public void SetConfig(string item, string value)
+            { 
+                ItemValue iv = new ItemValue();
+                iv.SetValue(this, item, value);
+            }
+            // 특정 item에 대한 value 조회
+            public string GetConfig(string item)
+            {
+                foreach (ItemValue iv in listConfig)
+                { 
+                    if (iv.GetItem() == item)
+                        return iv.GetValue();
+                }
+                return "";
+            }
+
+            private class ItemValue
+            {
+                private string item;
+                private string value;
+
+                public void SetValue(Configuration config, string item, string value)
+                {
+                    this.item = item;
+                    this.value = value;
+
+                    bool found = false;
+                    for (int i = 0; i < config.listConfig.Count; i++) {
+                        if (config.listConfig[i].item == item) {    // 기존 item이 있으면
+                            config.listConfig[i] = this;    // 기존 객체를 현재 객체(this)로 교체
+                            found = true;                   // 발견 표시 후 break
+                            break;
+                        }
+                    }
+                    if (found == false) 
+                        config.listConfig.Add(this);    // for문 종료 후 found가 false이면 현재 객체를 추가
+                    
+                }
+
+                public string GetItem()
+                { return item; }
+
+                public string GetValue() 
+                { return value; }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Configuration config = new Configuration();
+            config.SetConfig("Version", "V 5.0");
+            config.SetConfig("Size", "655.324 KB");
+
+            Console.WriteLine(config.GetConfig("Version")); // V 5.0
+            Console.WriteLine(config.GetConfig("Size"));    // 655.324 KB
+
+            config.SetConfig("Version", "V 5.0.1");
+            Console.WriteLine(config.GetConfig("Version")); // V 5.0.1
+        }
+    }
+}
---
+++ 7장/ex_PartialClass/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ex_PartialClass
+{
+    internal class Program
+    {
+
+        partial class MyClass 
+        {
+            public void Method1() {
+                Console.WriteLine("Method1");
+            }
+
+            public void Method2()
+            {
+                Console.WriteLine("Method2");
+            }
+        }
+
+        partial class MyClass
+        {
+            public void Method3()
+            {
+                Console.WriteLine("Method3");
+            }
+
+            public void Method4()
+            {
+                Console.WriteLine("Method4");
+            }
+        }
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+            obj.Method1();
+            obj.Method2();
+            obj.Method3();
+            obj.Method4();
+
+            /*
+            Method1
+            Method2
+            Method3
+            Method4
+             */
+
+            /*
+            분할 클래스
+            : 하나의 클래스를 여러 파일이나 여러 코드 블록으로 나눠서 정의
+            : 컴파일 시 모든 부분을 합쳐서 하나의 클래스로 만ㄷ ㅡㄹ어줌
+            
+            public partial class myclass(){}
+             */
+
+        }
+    }
+}

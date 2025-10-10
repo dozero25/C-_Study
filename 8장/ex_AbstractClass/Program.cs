66663f801a0f8599e43d11b7714eaa68e5c4f538
Yoon Do Yeong <ehdud05264@naver.com>
2025-10-10 21:43:11 +0900

    8장_추상클래스, 인터페이스

---
+++ 8장/ex_AbstractClass/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ex_AbstractClass
+{
+    abstract class AbstractBase
+    { 
+        protected void PrivateMethodA()
+        {
+            Console.WriteLine("AbstractBase.PrivateMethodA()");
+        }
+
+        public void PublicMethodA() {
+            Console.WriteLine("AbstractBase.PublicMethodA()");
+        }
+
+        public abstract void AbstractMehodA();
+    }
+
+    class Derived : AbstractBase
+    {
+        public override void AbstractMehodA()
+        {
+            Console.WriteLine("Derived.AbstractMehodA()");
+            PrivateMethodA();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractBase obj = new Derived();
+            obj.AbstractMehodA();
+            Console.WriteLine();
+            obj.PublicMethodA();
+
+            /*
+            Derived.AbstractMehodA()
+            AbstractBase.PrivateMethodA()
+
+            AbstractBase.PublicMethodA()
+             */
+        }
+    }
+}
---
+++ 8장/ex_DefaultImplementation/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ex_DefaultImplementation
+{
+    interface ILogger
+    {
+        void WriteLog(string message);
+
+        void WriteError(string error)
+        { 
+            WriteLog($"Error : {error}");
+        }
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        public void WriteLog(string message)
+        {
+            Console.WriteLine(
+                $"{DateTime.Now.ToLocalTime()}, {message}"
+                );
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger logger = new ConsoleLogger();
+            logger.WriteLog("System Up");       // 2025 - 10 - 10 금 오후 9:19:45, System Up
+            logger.WriteError("System Down");   // 2025-10-10 금 오후 9:19:45, Error: System Down
+
+            ConsoleLogger clogger = new ConsoleLogger();
+            clogger.WriteLog("System Up");      // 2025-10-10 금 오후 9:19:45, System Up
+            //clogger.WriteError("System Down"); 컴파일 에러 발생
+        }
+    }
+}
+
+
